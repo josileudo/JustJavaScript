94010e81f8faae0f586d0959dde8602301f9d946
Josileudo <josileudorodrigues75@gmail.com>
2021-10-05 16:51:53 -0300

    Correction state for true

--- Exercice/strictEquals.js
+++ Exercice/strictEquals.js
@@ -9,11 +9,11 @@ function strictEquals(a,b) {
         (Object.is(a , 0) && Object.is(b , -0)) || 
         (Object.is(a, -0) && Object.is(b , 0))
       ) {
-      return false
+      return true
     }
     return false
   }
 }
 
-const result = strictEquals(-0 , 1)
+const result = strictEquals(-0 , 0)
 console.log(result)
